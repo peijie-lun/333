29d7329571bc4e221f2d8aacc9edbb396ff9870d
peijie-L <ivychen940113@gmail.com>
2025-10-09 17:34:22 +0800

    修改參數格式

--- pages/api/line.js
+++ pages/api/line.js
@@ -47,12 +47,15 @@ export default async function handler(req, res) {
           replyMessage = '查詢失敗，請稍後再試。';
         }
 
-        await client.replyMessage(replyToken, [
-          {
-            type: 'text',
-            text: replyMessage,
-          },
-        ]);
+        // 確保訊息不是空字串
+        if (replyMessage && replyToken) {
+          await client.replyMessage(replyToken, [
+            {
+              type: 'text',
+              text: replyMessage,
+            },
+          ]);
+        }
       }
     }));
 
