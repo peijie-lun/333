dcc452ee4cb56e282ee0db326dc8f9694fd801cc
peijie-L <ivychen940113@gmail.com>
2025-10-30 21:42:33 +0800

    名稱變動

--- pages/api/line.js
+++ pages/api/line.js
@@ -10,8 +10,8 @@ const client = new Client(lineConfig);
 
 // ✅ Supabase 初始化
 const supabase = createClient(
-  process.env.SUPABASE_URL,
-  process.env.SUPABASE_ANON_KEY
+  process.env.NEXT_PUBLIC_SUPABASE_URL,
+  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
 );
 
 export const config = {
@@ -100,7 +100,7 @@ export default async function handler(req, res) {
             continue;
           }
 
-          const bubbles = imageData.map(img => ({
+          const bubbles = imageData.map(img => ({ 
             type: 'bubble',
             hero: {
               type: 'image',
