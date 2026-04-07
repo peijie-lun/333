d6d041ca4fef85bf5a0b99d08024310075fbc38e
peijie-L <ivychen940113@gmail.com>
2026-04-07 23:09:07 +0800

    888

--- app/api/iot-emergency-webhook/route.js
+++ app/api/iot-emergency-webhook/route.js
@@ -1,4 +1,4 @@
-import { supabase } from '@/utils/supabaseClient';
+import { supabase } from '../../../supabaseClient.js';
 import { Client } from '@line/bot-sdk';
 
 const lineClient = new Client({
